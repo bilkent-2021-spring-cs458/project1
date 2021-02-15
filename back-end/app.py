8c29cfc94da174437c3a167f099e298eb5a5cdf4
raupova <mokhlaroyim.raupova@soder.com.tr>
2021-02-16 02:08:58 +0300

    Added login and email verification

--- back-end/app.py
+++ back-end/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, request, redirect, render_template, url_for, session
+from flask import Flask, request, session
 from flask_mysqldb import MySQL, MySQLdb
 import bcrypt
+import re
 
 app = Flask(__name__)
 app.secret_key = b'Ziya the legend'
@@ -28,7 +29,15 @@ def signup():
     password = request_data['password']
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+    regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
+    prog = re.compile(regex)
+    if not prog.match(email):
+        return 'Invalid email address'
     cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
+    if cursor.fetchone() is not None:
+        return 'User already signed up'
+
     cursor.execute("INSERT INTO users (name,surname,email,password) VALUES(%s,%s,%s,%s)",
                    (name, surname, email, hashed_password,))
     mysql.connection.commit()
@@ -44,7 +53,17 @@ def login():
         password = request_data['password'].encode('utf-8')
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute("SELECT * FROM users WHERE VALUES email=%", email)
+        cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
+        user = cursor.fetchone()
+        cursor.close()
+
+        if len(user) > 0:
+            if bcrypt.hashpw(password, user['password'].encode('utf-8')) == user['password'].encode('utf-8'):
+                session[email] = email
+                return 'login is successful'
+
+            else:
+                return 'Email or password is wrong'
 
 
 @app.route('/logout', methods=["POST"])
