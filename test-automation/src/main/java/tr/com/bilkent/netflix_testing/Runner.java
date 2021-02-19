230cce9c86e36fac8b255e9c0e3597273deaedff
Ziya Mukhtarov <ziya.mukhtarov@ug.bilkent.edu.tr>
2021-02-20 00:11:00 +0400

    Added option to change the web driver

--- test-automation/src/main/java/tr/com/bilkent/netflix_testing/Runner.java
+++ test-automation/src/main/java/tr/com/bilkent/netflix_testing/Runner.java
@@ -1,16 +1,37 @@
 package tr.com.bilkent.netflix_testing;
 
+import java.util.Arrays;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 /**
  * Runs all the test sets and displays their results.
  */
 public class Runner {
 	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		TestSetResult overall = new TestSetResult(0, 0);
+		WebDriver driver;
+		System.out.println(Arrays.asList(args));
+		if (args.length < 1) {
+			System.out.println(
+					"Running with Chrome Driver. Path an argument to use another driver. Possible options: chrome, firefox, opera\n\n");
+			driver = new ChromeDriver();
+		} else {
+			switch (args[0]) {
+			case "firefox":
+				driver = new FirefoxDriver();
+				break;
+			case "opera":
+				driver = new OperaDriver();
+				break;
+			default:
+				driver = new ChromeDriver();
+			}
+		}
 
+		TestSetResult overall = new TestSetResult(0, 0);
 		TestSet[] sets = { new TestSet1(driver), new TestSet2(driver), new TestSet3(driver), new TestSet4(driver),
 				new TestSet5(driver) };
 
